5e09027d2a304c6bb345cd9e87ac53bd5a53adb9
yk-private3636 <hsnysk318@gmail.com>
2025-10-03 20:48:45 +0900

    first commit

---
+++ apps/api/app/src/health/health.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+
+@Controller('/health')
+export class HealthController {
+    constructor() {}
+    
+    @Get()
+    check() {
+        return {
+            text: 'health check ok'
+        }
+    }
+}
