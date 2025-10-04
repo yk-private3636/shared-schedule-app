204a6b019494522041e0a4d70c6efc069b2744a5
yk-private3636 <hsnysk318@gmail.com>
2025-10-04 17:44:26 +0900

    auth0導入検証(Googleのソーシャルログイン画面表示まで確認済み)

--- apps/web/app/src/pages/_app.tsx
+++ apps/web/app/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ''}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? ''}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
