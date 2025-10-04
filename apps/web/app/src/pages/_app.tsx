1c19a0941ccf9da83e756f2cbf63b7b94282e967
yk-private3636 <hsnysk318@gmail.com>
2025-10-04 22:25:28 +0900

    auth0導入検証(GoogleのソーシャルログインからJWT取得・検証まで確認)

--- apps/web/app/src/pages/_app.tsx
+++ apps/web/app/src/pages/_app.tsx
@@ -7,6 +7,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ''}
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? ''}
+      authorizationParams={{
+          redirect_uri: process.env.NEXT_PUBLIC_AUTH0_CALLBACK_URI ?? '',
+          audience: process.env.NEXT_PUBLIC_AUTH0_IDENTIFIER_AUD ?? ''
+        }}
     >
       <Component {...pageProps} />
     </Auth0Provider>
