7e4884af95a58992d2ef849637fb1e9ba6f19e96
yk-private3636 <hsnysk318@gmail.com>
2025-10-18 17:07:54 +0900

    OpenTelemetry導入設定

--- apps/api/app/src/main.ts
+++ apps/api/app/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import '@/instrumentation';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(process.env.APP_PORT ?? 8080);
 }
+
 bootstrap();
