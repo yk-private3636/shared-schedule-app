9c853bbf029e528e0fc4e5e3234500d56c466a6d
yk-private3636 <hsnysk318@gmail.com>
2025-10-22 13:03:06 +0900

    apiコンテナGraphQL導入、導入するにあたってJWT認可Guard処理もカスタマイズ

--- apps/api/app/src/healthz/health.controller.ts
+++ apps/api/app/src/healthz/health.controller.ts
@@ -1,9 +1,11 @@
+import { Public } from '@/authz/authz.module';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('/api/v1/health')
 export class HealthController {
   constructor() {}
 
+  @Public()
   @Get()
   check() {
     return {
