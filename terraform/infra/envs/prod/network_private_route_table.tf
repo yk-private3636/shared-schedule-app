effa3efb8f69836125ff23cd0d8c42dd78d0a079
yk-private3636 <hsnysk318@gmail.com>
2025-12-07 16:46:12 +0900

    Terraformファイル名の命名変更、不要箇所削除

--- terraform/infra/envs/prod/network_private_route_table.tf
+++ terraform/infra/envs/prod/network_private_route_table.tf
@@ -1,15 +1,11 @@
 module "private_route_table" {
   source = "../../modules/route_table"
 
-  # count = length(module.private_subnet)
-
-  # name   = "${local.route_table_private_name}-${count.index + 1}"
   name   = local.route_table_private_name
   vpc_id = module.vpc.id
 
   routes = [{
     cidr_block = "0.0.0.0/0"
-    # gateway_id             = module.ngw[count.index].id
     gateway_id             = module.ngw.id
     egress_only_gateway_id = ""
   }]
