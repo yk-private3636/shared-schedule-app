ea2ff9b2dc8a8d433adf4e94a9b6584b256cb2b3
yk-private3636 <hsnysk318@gmail.com>
2026-01-18 16:25:33 +0900

    ユーザードメイン単体テスト追加(プロフィール更新)

---
+++ apps/api/app/src/users/tests/user.domain.spec.ts
@@ -0,0 +1,57 @@
+import { UserStatus } from "@prisma/client";
+import { User } from "../domain/entities/user.entity";
+import { UserCannotBeUpdatedError } from "../domain/errors/UserCannotBeUpdatedError";
+
+describe("UserEntity", () => {
+  it("スタータスがACTIVEのユーザーはプロフィールを更新できる", () => {
+    // 準備
+    const sut = createUserWithStatus(UserStatus.ACTIVE);
+    const newEmail = "test+123@example.com";
+    const newFamilyName = "鈴木";
+    const newGivenName = "一郎";
+
+    // 実行
+    sut.updateProfile(newEmail, newFamilyName, newGivenName);
+
+    // 検証
+    expect(sut.getEmail()).toBe(newEmail);
+    expect(sut.getFamilyName()).toBe(newFamilyName);
+    expect(sut.getGivenName()).toBe(newGivenName);
+    expect(sut.getStatus()).toBe(UserStatus.ACTIVE);
+  });
+
+  it.each`
+    status
+    ${UserStatus.SUSPENDED}
+    ${UserStatus.BANNED}
+    ${UserStatus.DELETED}
+  `(
+    "ステータスが$statusのユーザーはプロフィールを更新できずエラーになる",
+    ({ status }: { status: UserStatus }) => {
+      // 準備
+      const sut = createUserWithStatus(status);
+      const newEmail = "test+123@example.com";
+      const newFamilyName = "鈴木";
+      const newGivenName = "一郎";
+
+      // 実行
+      const action = expect(() => {
+        sut.updateProfile(newEmail, newFamilyName, newGivenName);
+      });
+
+      // 検証
+      action.toThrow(UserCannotBeUpdatedError);
+    },
+  );
+});
+
+function createUserWithStatus(status: UserStatus): User {
+  return new User(
+    "f9d14968-a9f5-411d-811c-8a24189cdad0",
+    "google-oauth2|1234567890",
+    "test@example.com",
+    "山田",
+    "太郎",
+    status,
+  );
+}
